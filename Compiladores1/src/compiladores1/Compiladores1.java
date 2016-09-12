9924d7cf1bd678fe999ac4b73742cb551a32d373
LeonardoRez <leonardorec1@gmail.com>
2016-09-12 18:08:34 -0300

    trabalho entregue

--- Compiladores1/src/compiladores1/Compiladores1.java
+++ Compiladores1/src/compiladores1/Compiladores1.java
@@ -12,6 +12,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Tooltip;
 import javafx.scene.layout.AnchorPane;
@@ -29,21 +30,52 @@ public class Compiladores1 extends Application {
         Button btn = new Button();
         TextField infix = new TextField();
         infix.setTooltip(new Tooltip("INFIXA"));
+        infix.setMaxSize(160, 25);
+        infix.setMinSize(160, 25);
         TextField posfix = new TextField();
         posfix.setTooltip(new Tooltip("POSFIXA"));
+        posfix.setMaxSize(160, 25);
+        posfix.setMinSize(160, 25);
+        Label label = new Label();
+        label.setMaxSize(350, 20);
+        label.setMinSize(350, 20);
         btn.setText("Converter");
         btn.setOnAction(new EventHandler<ActionEvent>() {
 
             @Override
             public void handle(ActionEvent event) {
-                if (infix.getText().compareTo("") != 0) {
-                    posfix.setText(C.converter(infix.getText()));
+                try {
+                    if (C.conferePosfixa(C.converter(infix.getText()))) {
+                        posfix.setText(C.converter(infix.getText()));
+                        label.setText("");
+                    } else {
+                        label.setText("Expressão inválida!");
+                    }
+                } catch (Throwable e) {
+                    if (e.getMessage() == null) {
+                        label.setText("Expressão inválida!");
+                    } else {
+                        label.setText(e.getMessage());
+                    }
                 }
             }
         });
         infix.setOnAction((event) -> {
-            if (infix.getText().compareTo("") != 0) {
-                posfix.setText(C.converter(infix.getText()));
+            try {
+                if (infix.getText().compareTo("") != 0) {
+                    if (C.conferePosfixa(C.converter(infix.getText()))) {
+                        posfix.setText(C.converter(infix.getText()));
+                        label.setText("");
+                    } else {
+                        label.setText("Expressão inválida!");
+                    }
+                }
+            } catch (Throwable e) {
+                if (e.getMessage() == null) {
+                    label.setText("Expressão inválida!");
+                } else {
+                    label.setText(e.getMessage());
+                }
             }
         });
         AnchorPane root = new AnchorPane();
@@ -54,8 +86,11 @@ public class Compiladores1 extends Application {
         root.getChildren().get(1).setLayoutX(50);
         root.getChildren().get(1).setLayoutY(80);
         root.getChildren().add(btn);
-        root.getChildren().get(2).setLayoutX(210);
+        root.getChildren().get(2).setLayoutX(215);
         root.getChildren().get(2).setLayoutY(65);
+        root.getChildren().add(label);
+        root.getChildren().get(3).setLayoutX(5);
+        root.getChildren().get(3).setLayoutY(130);
 
         Scene scene = new Scene(root, 350, 150);
 
--- Compiladores1/src/compiladores1/model/Conversor.java
+++ Compiladores1/src/compiladores1/model/Conversor.java
@@ -11,20 +11,28 @@ public class Conversor {
         for (int i = 0; i < exp.length(); i++) { //ETAPA 1
             if (charValid(exp.charAt(i))) { //caso I
                 if (flag) { //se o concatenador está implícito
-                    while (!pilha.empty() && pilha.peek() == '*') {
-                        result += pilha.pop();
+                    while (!pilha.empty()) {
+                        if (pilha.peek() == '*' || pilha.peek() == '.') {
+                            result += pilha.pop();
+                        } else {
+                            break;
+                        }
                     }
                     pilha.push('.');
                     result += exp.charAt(i);
-                    flag = false;
+                    flag = true;
                 } else { //se é só um operando sem '.' antes
                     result += exp.charAt(i);
                     flag = true;
                 }
             } else if (exp.charAt(i) == '(') {//caso II
                 if (flag) {
-                    while (!pilha.empty() && pilha.peek() == '*') {
-                        result += pilha.pop();
+                    while (!pilha.empty()) {
+                        if (pilha.peek() == '*' || pilha.peek() == '.') {
+                            result += pilha.pop();
+                        } else {
+                            break;
+                        }
                     }
                     pilha.push('.');
                     flag = false;
@@ -37,30 +45,78 @@ public class Conversor {
                 pilha.pop(); //descarta o '('
                 flag = true;
             } else if (isOperador(exp.charAt(i))) { //caso IV
-                flag = false;
                 switch (exp.charAt(i)) {
                     case '+':
-                        while (!pilha.empty() && (pilha.peek() == '*' || pilha.peek() == '.')) {
-                            result += pilha.pop();
+                        flag = false;
+                        while (!pilha.empty()) {
+                            if (pilha.peek() == '*' || pilha.peek() == '.' || pilha.peek() == '+') {
+                                result += pilha.pop();
+                            } else {
+                                break;
+                            }
                         }
                         pilha.push('+');
                         break;
                     case '.':
-                        while (!pilha.empty() && pilha.peek() == '*') {
-                            result += pilha.pop();
+                        flag = false;
+                        while (!pilha.empty()) {
+                            if (pilha.peek() == '*' || pilha.peek() == '.') {
+                                result += pilha.pop();
+                            } else {
+                                break;
+                            }
                         }
                         pilha.push('.');
                         break;
                     case '*':
+                        while (!pilha.empty()) {
+                            if (pilha.peek() == '*') {
+                                result += pilha.pop();
+                            } else {
+                                break;
+                            }
+                        }
                         pilha.push('*');
+                        flag = true;
                         break;
                 }
             }
         }
         while (!pilha.empty()) {  //ETAPA 2
-            result += pilha.pop(); //limpa a pilha, colocando os operadores restantes na expressão
+            if (pilha.peek() != '(') {
+                result += pilha.pop(); //limpa a pilha, colocando os operadores restantes na expressão
+            } else {
+                throw new Error("Um '(' sobrou na pilha!");
+            }
         }
         return result;
+    }
+
+    public boolean conferePosfixa(String s) {
+        char op1, op2;
+        Stack<Character> pilha = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (charValid(s.charAt(i))) {
+                pilha.push(s.charAt(i));
+            } else //binario
+             if (s.charAt(i) == '+' || s.charAt(i) == '.') {
+                    op2 = pilha.pop();
+                    if (!pilha.empty()) {
+                        op1 = pilha.pop();
+                        pilha.push('0');
+                    } else {
+                        throw new IllegalAccessError("Operando não encontrado");
+                    }
+                } else {
+                    op1 = pilha.pop();
+                    pilha.push('0');
+                }
+        }
+        op1 = pilha.pop();
+        if (pilha.empty()) {
+            return true;
+        }
+        return false;
     }
 
     private static boolean charValid(char c) { //testa se é um símbolo válido
@@ -79,7 +135,17 @@ public class Conversor {
 
     public static void main(String[] args) { //main para testes
         Conversor c = new Conversor();
-        System.out.println(c.converter("A+BC(AA*B)*"));
+        String r = c.converter("(a+b)*aa");
+        System.out.println(r);
+        try {
+            if (c.conferePosfixa(r)) {
+                System.out.println("DEU CERTO");
+            } else {
+                System.out.println("DEU RUIM");
+            }
+        } catch (Throwable e) {
+            System.out.println(e.getMessage());
+        }
 
     }
 }
